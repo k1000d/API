063f8821985a5817e50f4aaa4cd90f012c773712
Camile Duhart <camile98.d@gmail.com>
2020-10-22 13:12:06 +0200

    webs

--- webs/web2.js
+++ webs/web2.js
@@ -1,26 +1,38 @@
 let request = new XMLHttpRequest()
 let stringfilm;
+let movies;
+function buscar() {
+    
+
+  movies = document.getElementById("movie").value;
+ 
+  
+
+
 
 
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
+  
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
-      stringfilm += `<h1>${movie.title}</h1>
+      stringfilm += ` 
+      <h1>${movie.title}</h1>
       <h3>${movie.description}</h3>
       <h3>Date: ${movie.release_date}</h3>`
     })
-    document.getElementById("movie-detail").innerHTML= stringfilm 
+    document.getElementById("movie-detail").innerHTML= stringfilm ;
   } else {
     console.log('error')
   }
   
 }
-function cambiarFondo(){
-  document.getElementById("body").body.style.backgroundColor = "blue"
-}
+ 
+ 
 
-request.send()
+ request.send()
+
+}
